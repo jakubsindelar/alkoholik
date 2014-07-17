ac778a2307cbc09f1babb7c0057137ec1df0b9f9
jakubsindelar <github@heroesofmemory.eu>
2014-07-17 19:16:57 +0200

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,9 @@ role :db,  %w{fanatik@heroesofmemory.eu}
 set :rails_env, "production"
 set :stage, 'production'
 set :application, 'web'
-set :repo_url, 'git@github.com:jakubsindelar/fiktivni_firma.git'
+set :repo_url, 'git@github.com:jakubsindelar/alkoholik.git'
 
 set :branch, "master"
-set :deploy_to, '/home/fanatik/dev/fiktivni_firma'
+set :deploy_to, '/home/fanatik/dev/alkoholik'
 
 server 'heroesofmemory.eu', user: 'fanatik', roles: %w{web app}
