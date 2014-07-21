695e3e785a63dd25094a8c6e12d11af12d32f27d
jakubsindelar <github@heroesofmemory.eu>
2014-07-22 00:01:27 +0200

    instalace přihlašování

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :names
+  devise_for :users
   resources :items
 
   resources :payments
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721150635) do
+ActiveRecord::Schema.define(version: 20140721215022) do
 
   create_table "items", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "names", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "names", ["email"], name: "index_names_on_email", unique: true, using: :btree
+  add_index "names", ["reset_password_token"], name: "index_names_on_reset_password_token", unique: true, using: :btree
 
   create_table "payments", force: true do |t|
     t.integer  "user_id"
@@ -43,6 +61,16 @@ ActiveRecord::Schema.define(version: 20140721150635) do
     t.string   "city"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
   end
 
 end
